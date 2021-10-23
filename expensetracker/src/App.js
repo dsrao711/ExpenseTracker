7f3ca5797b6e3c3afc737d6557ea1c837f0bbb6d
Divya <dsrao0712@gmail.com>
2021-10-24 01:12:08 +0530

    Expenses added

--- expensetracker/src/App.js
+++ expensetracker/src/App.js
@@ -1,39 +1,11 @@
 import './App.css';
-import ExpenseItem from './components/ExpenseItem/ExpenseItem';
-
+import Expenses from './components/Expenses/Expenses';
 
 function App() {
 
-  const expenses = [
-    {
-      title : 'Car Insurance',
-      amount : 'Rs. 10,000',
-      date : new Date(2021,2, 12)
-    },
-    {
-      title : 'Tuition Fee',
-      amount : 'Rs. 5000',
-      date : new Date(2021, 12, 1)
-    },
-    {
-      title : 'Mobile Repair',
-      amount : 'Rs . 120',
-      date : new Date(2021, 10, 22)
-    },
-    {
-      title : 'Gym membership',
-      amount : 'Rs. 1000',
-      date : new Date(2021, 10, 12)
-
-    }
-  ]
-  
   return (
     <div className="App">
-      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} />
-      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
-      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} />
-      <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date} />
+      <Expenses/>
     </div>
   );
 }
---
+++ expensetracker/src/components/Expenses/Expenses.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import ExpenseItem from '../ExpenseItem/ExpenseItem'
+import './Expenses.css'
+
+export default function Expenses() {
+
+    const expenses = [
+        {
+            title: 'Car Insurance',
+            amount: 'Rs. 10,000',
+            date: new Date(2021, 2, 12)
+        },
+        {
+            title: 'Tuition Fee',
+            amount: 'Rs. 5000',
+            date: new Date(2021, 12, 1)
+        },
+        {
+            title: 'Mobile Repair',
+            amount: 'Rs . 120',
+            date: new Date(2021, 10, 22)
+        },
+        {
+            title: 'Gym membership',
+            amount: 'Rs. 1000',
+            date: new Date(2021, 10, 12)
+
+        }
+    ]
+
+    return (
+        <div className = "expenses">
+            <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} />
+            <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
+            <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} />
+            <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date} />
+        </div>
+    )
+}
