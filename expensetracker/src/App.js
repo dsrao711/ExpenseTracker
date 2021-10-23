ab925f388cc21b7140ae69a238dbd701af1cf099
Divya <dsrao0712@gmail.com>
2021-10-23 18:23:53 +0530

    Expense Item

---
+++ expensetracker/src/App.js
@@ -0,0 +1,40 @@
+import './App.css';
+import ExpenseItem from './components/ExpenseItem/ExpenseItem';
+
+
+function App() {
+
+  const expenses = [
+    {
+      title : 'Car Insurance',
+      amount : 'Rs. 10,000',
+      date : new Date(2021, 12, 7)
+    },
+    {
+      title : 'Tuition Fee',
+      amount : 'Rs. 5000',
+      date : new Date(2021, 12, 1)
+    },
+    {
+      title : 'Mobile Repair',
+      amount : 'Rs . 120',
+      date : new Date(2021, 10, 22)
+    },
+    {
+      title : 'Gym membership',
+      amount : 'Rs. 1000',
+      date : new Date(2021, 10, 12)
+
+    }
+  ]
+  return (
+    <div className="App">
+      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} />
+      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
+      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} />
+      <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date} />
+    </div>
+  );
+}
+
+export default App;
---
+++ expensetracker/src/components/ExpenseItem/ExpenseItem.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './ExpenseItem.css'
+
+export default function ExpenseItem(props) {
+    return (
+        <div className="expense-item">
+            <div><b>📅 {props.date.toLocaleDateString()}</b></div>
+            <div className="expense-item__description">
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">💸 {props.amount}</div>
+            </div>
+        </div>
+    )
+}
