fdd5ec9f6f4db41543bccd56fb8baa0338e82143
Divya <dsrao0712@gmail.com>
2021-10-24 22:20:21 +0530

    trash commit

--- expensetracker/src/components/ExpenseItem/ExpenseItem.js
+++ expensetracker/src/components/ExpenseItem/ExpenseItem.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ExpenseDate from './ExpenseDate/ExpenseDate'
 import './ExpenseItem.css'
+import Card from '../Card/Card'
 
 export default function ExpenseItem(props) {
 
