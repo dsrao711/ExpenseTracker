27f8ce4cafc5723b59632557e495a2e4c78f3cb7
Divya <dsrao0712@gmail.com>
2021-10-25 14:11:44 +0530

    state naive approach

--- expensetracker/src/components/NewExpense/ExpenseForm/ExpenseForm.js
+++ expensetracker/src/components/NewExpense/ExpenseForm/ExpenseForm.js
@@ -3,24 +3,40 @@ import './ExpenseForm.css' ;
 
 const ExpenseForm = () => {
 
-    
+    // Naive approach
+
+    const [enteredTitle, setEnteredTitle] = useState('')
+    const [enteredAmount, setEnteredAmount] = useState('')
+    const [enteredDate, setEnteredDate] = useState('')
+
+    const titleChangeHandler = (event) => {
+        setEnteredTitle(event.target.value)
+    }
+
+    const AmountChangeHandler = (event) => {
+        setEnteredAmount(event.target.value)
+    }
+
+    const DateChangeHandler = (event) => {
+        setEnteredDate(event.target.value)
+    }
 
     return (
         <div className = "new-expense__control">
 
             <div className="new-expense__control">
                 <label>Title</label>
-                <input type="text" />
+                <input type="text" onChange = {titleChangeHandler}/>
             </div>
 
             <div className="new-expense__control">
                 <label>Amount</label>
-                <input type="number" min = "10" step = "10"  />
+                <input type="number" min = "10" step = "10" onChange = {AmountChangeHandler} />
             </div>
 
             <div className="new-expense__control">
                 <label>Date</label>
-                <input type="date" min = "2020-01-01" max = "2021-12-31" />
+                <input type="date" min = "2020-01-01" max = "2021-12-31" onChange = {DateChangeHandler} />
             </div>
 
         </div>
