12c0adb20505d398d7079223cd7e16ec644699a0
Divya <dsrao0712@gmail.com>
2021-10-27 22:58:18 +0530

    filter

--- expensetracker/src/components/Expenses/Expenses.js
+++ expensetracker/src/components/Expenses/Expenses.js
@@ -1,34 +1,45 @@
-import {React , useState} from 'react'
+import { React, useState } from 'react'
 import ExpensesFilter from '../ExpenseFilter/ExpenseFilter'
 import ExpenseItem from '../ExpenseItem/ExpenseItem'
 import './Expenses.css'
-    
+
 
 
 export default function Expenses(props) {
 
-    const [filteryear , setFilterYear] = useState('2021')
-    
+    const [filteryear, setFilterYear] = useState('2021')
+
 
     const filterByYear = (selectedYear) => {
         console.log(selectedYear)
         setFilterYear(selectedYear)
     }
 
-    
-    
+    const filteredExpenses = props.items.filter(expense => {
+        return expense.date.getFullYear().toString() === filteryear;
+    });
+
+    let expensesContent = <p>No expenses found</p>
+
+    if (filteredExpenses.length > 0) {
+        expensesContent = filteredExpenses.map((expense) => (
+            <ExpenseItem
+                key={expense.id}
+                title={expense.title}
+                amount={expense.amount}
+                date={expense.date}
+            />
+        ))
+    }
 
     return (
         <div>
             <div className="expenses">
-                <ExpensesFilter defaultYear = {filteryear} onFilterYear = {filterByYear} selected = {filteryear}/>
-                {props.items.filter((expense) => (expense.date.getFullYear().toString() === filteryear)).map(filtered_expense => (
-                        <ExpenseItem key = {filtered_expense.id} title = {filtered_expense.title} amount = {filtered_expense.amount} date = {filtered_expense.date} />
-                ))}        
+                <ExpensesFilter defaultYear={filteryear} onFilterYear={filterByYear} selected={filteryear} />
+                {expensesContent}
             </div>
         </div>
 
     )
 }
 
- 
